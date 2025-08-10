1fb694859c132dc92191a2b5a75cd810257c28e0
Dart2112 <dart2112@lapismc.net>
2025-08-10 13:32:19 +0930

    Working on a scoreboard implementation

--- src/main/java/net/lapismc/afkplusnobump/AFKPlusNoBump.java
+++ src/main/java/net/lapismc/afkplusnobump/AFKPlusNoBump.java
@@ -4,7 +4,6 @@ import net.lapismc.afkplus.AFKPlus;
 import net.lapismc.afkplus.api.AFKStartEvent;
 import net.lapismc.afkplus.api.AFKStopEvent;
 import net.lapismc.afkplus.playerdata.AFKPlusPlayer;
-import net.lapismc.afkplus.util.core.utils.LapisTaskHandler;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
@@ -12,20 +11,20 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scoreboard.Team;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
+import java.util.*;
 
 public final class AFKPlusNoBump extends JavaPlugin implements Listener {
 
     List<LivingEntity> bumpRemovedEntities = new ArrayList<>();
+    HashMap<UUID, Team.OptionStatus> previousCollisionStatus = new HashMap<>();
 
     @Override
     public void onEnable() {
         //Start a reaping task that will handle collision settings
-        LapisTaskHandler.LapisTask repeatingTask = AFKPlus.getInstance().tasks.runTaskTimer(this::runCollisionTask, 10, 10, false);
-        AFKPlus.getInstance().tasks.addTask(repeatingTask);
+        //LapisTaskHandler.LapisTask repeatingTask = AFKPlus.getInstance().tasks.runTaskTimer(this::runCollisionTask, 10, 10, false);
+        //AFKPlus.getInstance().tasks.addTask(repeatingTask);
         //Add a listener for AFK start and stop events
         Bukkit.getPluginManager().registerEvents(this, this);
         getLogger().info(getName() + " v." + getDescription().getVersion() + " has been enabled!");
@@ -45,8 +44,29 @@ public final class AFKPlusNoBump extends JavaPlugin implements Listener {
         Player p = Bukkit.getPlayer(e.getPlayer().getUUID());
         if (p == null)
             return;
-        p.setCollidable(false);
-        bumpRemovedEntities.add(p);
+        //TODO: Something in here breaks when using the TAB plugin
+        // Need to debug to see what teams are being set and what the player actually has
+        Team playersTeam = p.getScoreboard().getPlayerTeam(p);
+        if (playersTeam == null) {
+            playersTeam = p.getScoreboard().getTeam("AFKPlayers");
+            if (playersTeam == null) {
+                playersTeam = p.getScoreboard().registerNewTeam("AFKPlayers");
+                playersTeam.setOption(Team.Option.COLLISION_RULE, Team.OptionStatus.NEVER);
+            }
+        }
+        //By here we definitely have a team
+        //Skip setting options if we have the AFKPlayers team
+        //But make sure we add them to the team
+        if (playersTeam.getName().equals("AFKPlayers")) {
+            playersTeam.addEntry(p.getName());
+            return;
+        }
+        //Store the collide setting of said team
+        previousCollisionStatus.put(p.getUniqueId(), playersTeam.getOption(Team.Option.COLLISION_RULE));
+        //Set the option to never
+        playersTeam.setOption(Team.Option.COLLISION_RULE, Team.OptionStatus.NEVER);
+        //p.setCollidable(false);
+        //bumpRemovedEntities.add(p);
     }
 
     @EventHandler
@@ -54,8 +74,20 @@ public final class AFKPlusNoBump extends JavaPlugin implements Listener {
         Player p = Bukkit.getPlayer(e.getPlayer().getUUID());
         if (p == null)
             return;
-        p.setCollidable(true);
-        bumpRemovedEntities.remove(p);
+        //Get the players team and return if it is null, this means we didn't set the team right or something interfered
+        Team playersTeam = p.getScoreboard().getPlayerTeam(p);
+        if (playersTeam == null)
+            return;
+        if (playersTeam.getName().equals("AFKPlayers")) {
+            //If it's the AFKPlayers team, simply remove them from it
+            playersTeam.removeEntry(p.getName());
+        } else {
+            //Otherwise restore the teams collision rule
+            playersTeam.setOption(Team.Option.COLLISION_RULE, previousCollisionStatus.get(p.getUniqueId()));
+            previousCollisionStatus.remove(p.getUniqueId());
+        }
+        //p.setCollidable(true);
+        //bumpRemovedEntities.remove(p);
     }
 
     public void runCollisionTask() {
@@ -74,9 +106,12 @@ public final class AFKPlusNoBump extends JavaPlugin implements Listener {
             //Continue if the player isn't online
             if (p == null)
                 continue;
+            //Make sure the player is still included by adding them here
+            entitiesToProcess.add(p);
             //Find entities within 2 blocks
             List<Entity> nearbyEntities = p.getNearbyEntities(4, 4, 4);
             for (Entity e : nearbyEntities) {
+                //Make sure it's a living entity
                 if (!(e instanceof LivingEntity))
                     continue;
                 //It's a living entity within two blocks
